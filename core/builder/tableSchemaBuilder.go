243636269453e1dbec13f7ef9e6089ea54641236
v_zhilovgli <v_zhilovgli@tencent.com>
2023-12-28 18:06:30 +0800

    update:动态获取表格名称

--- core/builder/tableSchemaBuilder.go
+++ core/builder/tableSchemaBuilder.go
@@ -141,13 +141,14 @@ func (b *TableSchemaBuilder) BuildFromSQL(sql string) (*schema.TableSchema, erro
 	return tableSchema, nil
 }
 
-// BuildFromExcel 导入 Excel TODO Sheet1 是硬编码，改为入参，
+// BuildFromExcel 导入 Excel TODO sheetMap[1] 是硬编码，可以改为入参，
 func (b *TableSchemaBuilder) BuildFromExcel(file multipart.File) (*schema.TableSchema, error) {
 	xlsx, err := excelize.OpenReader(file)
 	if err != nil {
 		return nil, fmt.Errorf("表格解析错误: %v", err)
 	}
-	rows, err := xlsx.Rows("Sheet1")
+	sheets := xlsx.GetSheetMap()
+	rows, err := xlsx.Rows(sheets[1])
 	if err != nil {
 		return nil, fmt.Errorf("表格无数据: %v", err)
 	}
